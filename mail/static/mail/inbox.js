dd2ffc630ba930d71ba86cbb5d03acec1a9a9b12
Miguel Pereira <miguelpereira2000@gmail.com>
2021-09-02 23:24:52 +0100

    up

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -23,11 +23,100 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get emails
+  fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      // ... do something else with emails ...
+      // TODO
+    });
+}
+
+function read_email(id) {
+  // Show the mailbox and hide other views
+  document.querySelector('#read-email-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Get email
+  fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(email => {
+      // Print email
+      console.log(email);
+
+      // ... do something else with email ...
+      // TODO
+    });
+
+  // Mark email as read
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  })
+}
+
+function unread_email(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: false
+    })
+  })
+
+  // UI update
+  // TODO
+
+}
+
+function archive_email(id) {
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: true
+    })
+  })
+
+  // UI update
+  // TODO
+
+}
+
+function send_email() {
+  // DOM get variables content
+  // TODO
+  var recipients;
+  var subject;
+  var body;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+
+      // verify if email exists 
+      // show result success
+      // TODO
+
+    });
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+python3 manage.py makemigrations mail
+python3 manage.py migrate
+
+# RUN
+   python3 manage.py runserver
